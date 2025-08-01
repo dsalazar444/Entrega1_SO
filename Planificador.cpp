58990bedeb71ddf37df6d5b0f252db664730b32c
Daniela Salazar Amaya <dsalazara1@eafit.edu.co>
2025-07-31 20:15:27 -0500

    inicios de inciso c)

--- Planificador.cpp
+++ Planificador.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <typeinfo>
+#include <cstdio>
 #include "imports/json.hpp"
 #include "Proceso.cpp"
 
@@ -24,7 +25,31 @@ struct Planificador {
     }
 
     void recorrerProcesos(queue<Proceso> procesos) {
-        cout << "Tipo " << typeid(procesos.front()).name() << endl;
+        //cout << "Tipo " << typeid(procesos.front()).name() << endl;
+
+    }
+
+    void roundRobin(){
+        int n = this->procesos.size();
+        while(n > 0 ){
+            this->procesos.push(this->procesos.front()); //Añadimos primer elemento a final de la cola
+            Proceso actual = this->procesos.front(); //Obtengo el primer elemento
+            printf("Cargando estado de Proceso %d: PC= %d, AX= %d, BX= %d, CX= %d\n", actual.pid, actual.pc, actual.ax, actual.bx, actual.cx);
+            //OJO: El número de nuestro proceso será el que se le asigno en el JSON
+            funcionTiempo();
+            printf("Guardando estado de Proceso %d: PC= %d, AX= %d, BX= %d, CX= %d\n", actual.pid, actual.pc, actual.ax, actual.bx, actual.cx);
+            //ESE PRINT puede ir en Finstruccion (donde se actualizan los datos
+            //del proceso)
+            this->procesos.pop(); //Eliminamos el primer elemento, avanzando en la queue
+            n--; //Para que solo lo recorra un ciclo (porque no eliminaremos elementos)
+        }
+        
+
+    }
+
+    void funcionTiempo(){
+        // TO-DO: Implementar lógica de tiempo-> counter que llama tarea durante
+        // n segundos. OJO, Ftiempo -> Finstruccion -> debe actualizar los ficheros
     }
 
 };
--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 int main() {
     Planificador p("./documento.JSON");
-    p.recorrerProcesos(p.procesos);
+    //p.recorrerProcesos(p.procesos); //-> Era para verificar que se estuvieran
+    //agregando a la queu como dato Proceso
+    p.roundRobin();
     return 0;
 }
