df18bea19eec313fc61fb2875808676dcd8fe6c8
Daniela Salazar Amaya <141360911+dsalazar444@users.noreply.github.com>
2025-08-08 20:36:43 -0500

    Update main.cpp
    main.cpp comentado

--- main.cpp
+++ main.cpp
@@ -7,16 +7,19 @@ int main() {
     string filepath;
     cin >> filepath;*/
 
+    //Obtenemos los procesos del txt
     p.agregarTodosDesdeTxt("procesos.txt");
 
     cout << "\n=== Todos los procesos cargados desde TXT ===\n";
     queue<Proceso> copia = p.procesos;
 
+    //Imprimimos copia de los procesos cargados del txt
     while (!copia.empty()) {
         copia.front().mostrar();
         copia.pop();
     }
 
+    //Llamamos al round Robin
     p.roundRobin();
     return 0;
 }
